a5ba822b6aab98e9389301fd4d1d86cc3cbf03de
Mace Akkari <mace.akkari@relyfe.com>
2023-01-04 13:21:46 +0000

    add seprate sections for each page

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use("/articles", articleRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello there");
+  const articles = [
+    {
+      title: "Test Title",
+      createdAt: new Date(),
+      description: "Test Description",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
 });
 
 app.listen(8080);
