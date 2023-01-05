acaceb32e5ef7f6360aeb7330219957550797cc0
Mace Akkari <mace.akkari@relyfe.com>
2023-01-05 13:22:11 +0000

    add comment

--- routes/articles.js
+++ routes/articles.js
@@ -9,7 +9,7 @@ router.get("/new", (req, res) => {
 router.get("/:id", async (req, res) => {
   const article = await Article.findById(req.params.id);
   if (article == null) res.redirect("/");
-  res.render("articles/show", { article });
+  res.render("articles/show", { article }); // can i make this a ternary?
 });
 
 router.post("/", async (req, res) => {
